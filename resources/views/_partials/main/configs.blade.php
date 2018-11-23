74794474eb5ee58868232cf0f20900e7bbec7b47
Hilkiah Makemo <hilmak01@gmail.com>
2018-11-23 12:56:36 -0700

    Updated configs blade view

--- resources/views/_partials/main/configs.blade.php
+++ resources/views/_partials/main/configs.blade.php
@@ -9,16 +9,16 @@
                 <a style="background: #cbebf7;color: #37aecc;" class="btn btn-block" href="#" title="light Blue color">edit this page</a>
             </li>
             <li>
-                <a style="background: #32374b;color: #32374b" class="changecolor oxford-text" href="#" title="Oxford color">---</a>
+                <a style="background: #32374b;color: #f2f2f2" class="btn btn-block" href="#" title="Oxford color">update content</a>
             </li>
             <li>
-                <a style="background: #a1cf68;color: #a1cf68" class="changecolor green-text" href="#" title="Green color">---</a>
+                <a style="background: #a1cf68;color:#ffffff" class="btn btn-block" href="#" title="Green color">unpublish page</a>
             </li>
             <li>
-                <a style="background: #FF7619;color: #FF7619" class="changecolor orange-text" href="#" title="Orange color">---</a>
+                <a style="background: #FF7619;color: #FF7619" class="btn btn-block" href="#" title="Orange color">---</a>
             </li>
             <li>
-                <a style="background: #7300ab;color: #7300ab" class="changecolor purple-text" href="#" title="Purple color">---</a>
+                <a style="background: #7300ab;color: #7300ab" class="btn btn-block" href="#" title="Purple color">---</a>
             </li>
         </ul>
     </div>
