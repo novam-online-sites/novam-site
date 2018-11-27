6609777531038817fc7079c58acaace18022065a
Hilkiah Makemo <hilmak01@gmail.com>
2018-11-26 23:57:48 -0700

    Updated forms

--- resources/views/forms/messages/create.blade.php
+++ resources/views/forms/messages/create.blade.php
@@ -1,4 +1,4 @@
-<form action="{{ route('messages') }}" method="POST">
+<form action="/messages" method="POST">
     @csrf
     <div class="form-group">
         <input type="text" name="title" class="form-control" />
