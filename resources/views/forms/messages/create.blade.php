7871dcd2f54775a1445a9cdad126fe0055209548
Hilkiah Makemo <hilmak01@gmail.com>
2018-11-26 22:32:26 -0700

    dashboard

---
+++ resources/views/forms/messages/create.blade.php
@@ -0,0 +1,17 @@
+<form action="{{ route('messages') }}" method="POST">
+    @csrf
+    <div class="form-group">
+        <input type="text" name="title" class="form-control" />
+    </div>
+    <div class="form-group">
+        <textarea name="message" class="form-control" rows="4"></textarea>
+    </div>
+    <div class="form-row">
+        <div class="col-md-7">
+            <input type="file" name="files" class="form-file" />>
+        </div>
+        <div class="col">
+            <button type="submit" class="btn btn-primary btn-block">Send</button>
+        </div>
+    </div>
+</form>
