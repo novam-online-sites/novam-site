a55b3413f844f3b3b3651a22737910b959f1b87a
Hilkiah Makemo <HILMAK01@GMAIL.COM>
2018-11-27 18:41:33 -0700

    Updated views

--- resources/views/_partials/main/siteNav.blade.php
+++ resources/views/_partials/main/siteNav.blade.php
@@ -1,12 +1,33 @@
+
+@php
+    $Pages = [
+        '/about' => 'About Us',
+        '/services' => 'Services',
+        '/offering' => 'Our Tools',
+        '/portfolio' => 'Portfolio',
+        '/blog' => 'Our Blog',
+    ];
+@endphp
 <ul>
-    @isset($home)
-    <li><a href="/">Home</a></li>
-    @endisset
-    <li><a href="/about">About Us</a></li>
-    <li><a href="/services">Services</a></li>
-    <li><a href="/offering">Web Tools</a></li>
-    <li><a href="/portfolio">Portfolio</a></li>
-    <li><a href="/blog">Our Blog</a></li>
+    @foreach ($Pages as $url => $Label)
+    <li {!! $class ?? '' !!}>
+        <a{!! request()->is("*$url*")? ' class="active"': '' !!} href="{{ $url }}">{{ $Label }}</a>
+    </li>
+    @endforeach
+    {{-- @isset($home)
+    <li{!! $class ?? '' !!}>
+        <a class="{{ request()->is($url = '/')? 'active': '' }}" href="{{ $url }}">{{ $page }}</a></li>
+    @endisset --}}
+    {{-- <li{!! $class ?? '' !!}>
+        <a class="{{ request()->is($url = 'about')? 'active': '' }}" href="{{ $url }}">About</a></li>
+    <li{!! $class ?? '' !!}>
+        <a class="{{ request()->is($url = 'services')? 'active': '' }}" href="/services">{{ $url }}</a></li>
+    <li{!! $class ?? '' !!}>
+        <a class="{{ request()->is($url = 'offering')? 'active': '' }}" href="/offering">{{ $url }}</a></li>
+    <li{!! $class ?? '' !!}>
+        <a class="{{ request()->is($url = 'portfolio')? 'active': '' }}" href="/portfolio">{{ $url }}</a></li>
+    <li{!! $class ?? '' !!}>
+        <a class="{{ request()->is($url = 'blog')? 'active': '' }}" href="/blog">{{ $url }}</a></li> --}}
     {{-- <li class="dropdown">
         <a href="#">Our Blog</a>
         <ul class="submenu">
