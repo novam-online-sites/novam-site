f27ea45a10727f4e4504136e8582af34fe3af562
Hilkiah Makemo <HILMAK01@GMAIL.COM>
2018-11-30 21:59:24 -0700

    Updated maintenance mode ips

--- app/Http/Middleware/CheckForMaintenanceMode.php
+++ app/Http/Middleware/CheckForMaintenanceMode.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware;
 
+use Closure;
 use Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode as Middleware;
 
 class CheckForMaintenanceMode extends Middleware
