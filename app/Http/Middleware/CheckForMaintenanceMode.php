6c011d9e8157992e821993ae9885058e82b5abf0
Hilkiah Makemo <hilkiah.makemo@skywest.com>
2018-12-06 08:49:15 -0700

    Removed IPs from maintenance mode check

--- app/Http/Middleware/CheckForMaintenanceMode.php
+++ app/Http/Middleware/CheckForMaintenanceMode.php
@@ -17,11 +17,11 @@ class CheckForMaintenanceMode extends Middleware
         "account/request"
     ];
 
-    public function handle($request, Closure $next)
-    {
-        if(!in_array($request->ip(), config('view.ips'))){
-            return abort(503);
-        }
-        return $next($request);
-    }
+    // public function handle($request, Closure $next)
+    // {
+    //     if(!in_array($request->ip(), config('view.ips'))){
+    //         return abort(503);
+    //     }
+    //     return $next($request);
+    // }
 }
